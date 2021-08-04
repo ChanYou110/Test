709227a701504b3a3c539c8d3a78bdcf852f5056
ChanYou110 <mushoku110@gmail.com>
2021-08-04 15:24:40 +0900

    カリキュラム08-4-4

--- blog/resources/views/create.blade.php
+++ blog/resources/views/create.blade.php
@@ -13,11 +13,13 @@
             {{ csrf_field() }}
             <div class="title">
                 <h2>タイトル</h2>
-                <input type="text" name="post[title]" placeholder="タイトル"/>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old("post.title") }}"/>
+                <p class="title_error" style="color:red">{{ $errors->first("post.title") }}</p>
             </div>
             <div class="body">
                 <h2>メッセージ</h2>
-                <textarea name="post[body]" placeholder="メッセージ"></textarea>
+                <textarea name="post[body]" placeholder="メッセージ">{{ old("post.body") }}</textarea>
+                <p class="body_error" style="color:red">{{ $errors->first("post.body") }}</p>
             </div>
             <input type="submit" value="登録"/>
         </form>
